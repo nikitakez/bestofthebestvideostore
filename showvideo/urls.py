5b1896d04df00fd47bc0b5501234e3f8ec493a29
Arastovich <torzhetski@mail.ru>
2020-06-13 12:50:10 +0300

    add video content

--- showvideo/urls.py
+++ showvideo/urls.py
@@ -7,7 +7,7 @@ from . import  views
 urlpatterns = [
     re_path("123/", views.hello),
     re_path("456/", views.world, name ="main_page"),
-    re_path("comment/(?P<id>\d+)/", views.accept_comment),
+    re_path("comment/(?P<id>\d+)/", views.accept_comment, name="add_comment"),
     re_path("one_video/(?P<id>\d+)/", views.one_video),
     re_path("add_like/(?P<id>\d+)/", views.add_like),
     re_path("ajax/add_like", views.ajax_like)
